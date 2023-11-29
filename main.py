0fdc7efee4cdd53bf3d63d2249708aa0974a4768
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-11-28 21:53:33 -0600

    UPDATE main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ conn = sqlite3.connect("sql/devices.db")
 app = fastapi.FastAPI()
 
 origins = [
-    "https://api-dispositivos-frontend-xxxxxxxxx.herokuapp.com"  # Cambia la URL según tu frontend de dispositivos
+    "http://127.0.0.1:8080"  # Cambia la URL según tu frontend de dispositivos
 ]
 
 app.add_middleware(
