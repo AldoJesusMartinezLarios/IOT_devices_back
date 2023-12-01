e91161a3309ee6d078a59839bcd3221ab30f63a1
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-12-01 09:49:22 -0600

    UPLOADS files

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# IOT_devices_backend
-BACKEND for iot devices
+# API_IOT
+Api rest para poder interactuar con dispositivos IOT
+
+# Heroku SQLite3, FastAPI demo
+Heroku SQLite3, FastAPI demo
 
 gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
--- main.py
+++ main.py
@@ -3,88 +3,64 @@ import sqlite3
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-# Conecta a la base de datos
+# Crea la base de datos
 conn = sqlite3.connect("sql/dispositivos.db")
 
 app = fastapi.FastAPI()
 
-# Permitimos los origenes para conectarse
 origins = [
-    "http://0.0.0.0:8080",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "https://wokwi.com/projects/382924417699279873",
-    "https://iotfrontend-76a7ab26b9a7.herokuapp.com"
+    "https://8080-axelcarrill-iotfrontwok-lkrluqs09jk.ws-us106.gitpod.io"
 ]
 
-# Agregamos las opciones de origenes, credenciales, métodos y headers
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins= ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class Dispositivo(BaseModel):
-    dispositivo: str
+    id : int
+    dispositivo : str
     valor: int
 
-@app.post("/iot")
-async def crear_dispositivo(dispositivo: Dispositivo):
-    """Crea un nuevo dispositivo."""
+class DispositivoPUT(BaseModel):
+    valor: int
+
+
+@app.get("/")
+async def root():
+    return "Conexion exitosa"
+
+@app.get("/dispositivos")
+async def get_all():
+    """Obtiene todos los contactos."""
     c = conn.cursor()
-    c.execute('INSERT INTO iot (dispositivo, valor) VALUES (?, ?)',
-              (dispositivo.dispositivo, dispositivo.valor))
+    c.execute('SELECT * FROM dispositivos;')
+    response = []
+    for row in c:
+        dispositivo = {"id":row[0],"dispositivo":row[1], "valor":row[2]}
+        response.append(dispositivo)
+    return response
+
+@app.get("/dispositivos/{id}")
+async def get_dispositivo(id: int):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
+    c = conn.cursor()
+    c.execute('SELECT * FROM dispositivos WHERE id = ?', (id,))
+    dispositivo = None
+    for row in c:
+         dispositivo = {"id":row[0],"dispositivo":row[1], "valor":row[2]}
+    return dispositivo
+
+@app.put("/dispositivos/{id}")
+async def get_dispositivo(id: int, dispositivo: DispositivoPUT):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
+    c = conn.cursor()
+    c.execute('UPDATE dispositivos SET valor = ? WHERE id = ?;',
+              (dispositivo.valor,id))
     conn.commit()
-    new_dispositivo_id = c.lastrowid  # Obtén el ID generado automáticamente
-    return {"id": new_dispositivo_id, "dispositivo": dispositivo.dispositivo, "valor": dispositivo.valor}
-
-
-@app.get("/iot")
-async def obtener_dispositivos():
-    """Obtiene todos los dispositivos."""
-    c = conn.cursor()
-    c.execute('SELECT * FROM iot')
-    dispositivos = []
-    for row in c.fetchall():
-        dispositivo = {"id": row[0], "dispositivo": row[1], "valor": row[2]}
-        dispositivos.append(dispositivo)
-    return dispositivos
-
-@app.get("/iot/{id}")
-async def obtener_dispositivo(id: int):
-    """Obtiene un dispositivo por su ID."""
-    c = conn.cursor()
-    c.execute('SELECT * FROM iot WHERE id = ?', (id,))
-    row = c.fetchone()
-    if row:
-        dispositivo = {"id": row[0], "dispositivo": row[1], "valor": row[2]}
-        return dispositivo
-    else:
-        return {"error": -1}
-
-@app.patch("/iot/{id}/{valor}")
-async def actualizar_dispositivo(id: int, valor: int):
-    """Actualiza el valor de un dispositivo."""
-    # Verifica si el dispositivo existe antes de actualizar
-    c = conn.cursor()
-    c.execute('SELECT * FROM iot WHERE id = ?', (id,))
-    row = c.fetchone()
-    if not row:
-        return {"error": -1}
-
-    # Actualiza el valor del dispositivo en la base de datos
-    c.execute('UPDATE iot SET valor = ? WHERE id = ?', (valor, id))
-    conn.commit()
-
-    # Retorna el resultado
-    return {"valor": valor}
-
-@app.delete("/iot/{id}")
-async def eliminar_dispositivo(id: int):
-    """Elimina un dispositivo."""
-    c = conn.cursor()
-    c.execute('DELETE FROM iot WHERE id = ?', (id,))
-    conn.commit()
-    return {"message": "Dispositivo eliminado"}
+    return dispositivo
